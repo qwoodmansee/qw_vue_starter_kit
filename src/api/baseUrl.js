fc990b1272574777a34b991bfc049163279b9fd9
DESKTOP-Q1KJN8C\Quinton <qwoodmansee@capital.edu>
2018-02-05 20:41:41 -0500

    added ability to switch to mock api with ?useMockApi=true

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,14 @@
 export default function getBaseUrl() {
-    const inDevelopment = window.location.hostname === "localhost";
-    return inDevelopment ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/'
+}
+
+// Does what it's name says it does, got from Cory House who got it from stack overflow
+function getQueryStringParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
